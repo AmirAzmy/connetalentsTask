92c709b534c91a0fb264f9185fa3602b644b980d
amir <eng.amirazmys@gmail.com>
2024-03-25 23:56:25 +0200

    Add hotel request validation and hotel controller

---
+++ app/Http/Controllers/HotelController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\HotelRequest;
+use Illuminate\Http\Request;
+
+class HotelController extends Controller
+{
+    public function index(HotelRequest $request)
+    {
+
+    }
+}
